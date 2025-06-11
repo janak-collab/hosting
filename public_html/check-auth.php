42fdf256a40fea7bfaa431ca61b398cf701fcab4
Janak Vidyarthi <janak.vidyarthi@gmail.com>
2025-06-11 17:54:10 +0000

    Auto-sync on 2025-06-11 17:54:10

--- public_html/check-auth.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo "PHP_AUTH_USER: " . ($_SERVER['PHP_AUTH_USER'] ?? 'not set') . "\n";
-echo "REMOTE_USER: " . ($_SERVER['REMOTE_USER'] ?? 'not set') . "\n";
-echo "REMOTE_ADDR: " . $_SERVER['REMOTE_ADDR'] . "\n";
--- public_html/dictation_redirect.php
+++
@@ -1,5 +0,0 @@
-<?php
-// This file handles /dictation requests
-// Just include the actual dictation.php file
-require_once __DIR__ . '/dictation.php';
-?>
