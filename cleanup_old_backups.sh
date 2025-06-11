d6099a22823b8dee71383b8d003102934cc1a368
Janak Vidyarthi <janak.vidyarthi@gmail.com>
2025-06-11 17:47:27 +0000

    Auto-sync on 2025-06-11 17:47:27

--- cleanup_old_backups.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Keep only last 7 days of backups
-find ~/backups/daily -name "*.tar.gz" -mtime +7 -delete
-# Keep only last 30 days in archive
-find ~/backups/archive -name "*.tar.gz" -mtime +30 -delete
