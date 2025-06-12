24c2ecdd101e7ae26daf18c3982d394dc44078a6
Janak Vidyarthi <janak.vidyarthi@gmail.com>
2025-06-12 00:31:55 +0000

    Clean up assets: Remove duplicate min files, update references
    - Removed app.min.css and app.min.js (were duplicates of non-min versions)
    - Updated base.php layout to reference app.css/app.js instead of min versions
    - Cleaned up outdated files in app/resources/assets/
    - All templates now consistently use app.css and app.js

--- app/resources/views/layouts/base.php
+++ app/resources/views/layouts/base.php
@@ -6,11 +6,11 @@
     <title><?php echo isset($title) ? htmlspecialchars($title) . ' - ' : ''; ?>Greater Maryland Pain Management</title>
     
     <!-- Preload critical assets -->
-    <link rel="preload" href="<?php echo asset('css/app.min.css'); ?>" as="style">
-    <link rel="preload" href="<?php echo asset('js/app.min.js'); ?>" as="script">
+    <link rel="preload" href="<?php echo asset('css/app.css'); ?>" as="style">
+    <link rel="preload" href="<?php echo asset('js/app.js'); ?>" as="script">
     
     <!-- Main stylesheet -->
-    <link rel="stylesheet" href="<?php echo asset('css/app.min.css'); ?>">
+    <link rel="stylesheet" href="<?php echo asset('css/app.css'); ?>">
     
     <!-- Favicon -->
     <link rel="icon" type="image/x-icon" href="<?php echo asset('images/favicon.ico'); ?>">
@@ -29,7 +29,7 @@
     <?php echo $content; ?>
     
     <!-- Main JavaScript -->
-    <script src="<?php echo asset('js/app.min.js'); ?>"></script>
+    <script src="<?php echo asset('js/app.js'); ?>"></script>
     
     <!-- Page-specific scripts -->
     <?php if (isset($scripts)): ?>
